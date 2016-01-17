b26890f1d36f64467d4952c317358751733e5edf
Andres Sapper <asapper@hotmail.com>
2016-01-16 19:37:05 -0600

    add generic views to Personal app

--- personal/urls.py
+++ personal/urls.py
@@ -2,6 +2,14 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'personal'
 urlpatterns = [
+    # ex: /personal/
     url(r'^$', views.index, name='index'),
+    # ex: /personal/clients/
+    url(r'^clients/$', views.ClientsView.as_view(), name='clients'),
+    # ex: /personal/executives/
+    url(r'^executives/$', views.ExecutivesView.as_view(), name='executives'),
+    # ex: /personal/persons/
+    url(r'^persons/$', views.PersonsView.as_view(), name='persons'),
 ]
--- personal/views.py
+++ personal/views.py
@@ -1,5 +1,35 @@
 from django.http import HttpResponse
+from django.views import generic
+
+from .models import Client, Executive, Person
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the 'Personal' index.")
+    return HttpResponse("Hello, world. You're at the 'Personal' app index.")
+
+
+class ClientsView(generic.ListView):
+    template_name = 'personal/clients.html'
+    context_object_name = 'latest_client_list'
+
+    def get_queryset(self):
+        """Return all the Clients."""
+        return Client.objects.all()
+
+
+class ExecutivesView(generic.ListView):
+    template_name = 'personal/executives.html'
+    context_object_name = 'latest_executive_list'
+
+    def get_queryset(self):
+        """Return all the Executives."""
+        return Executive.objects.all()
+
+
+class PersonsView(generic.ListView):
+    template_name = 'personal/persons.html'
+    context_object_name = 'latest_person_list'
+
+    def get_queryset(self):
+        """Return all the Persons."""
+        return Person.objects.all()
