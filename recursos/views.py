b1c2d902156ce3934bacfebf33775bfb6441957d
Andres Sapper <asapper@hotmail.com>
2016-01-17 19:00:40 -0600

    add Recursos generic views and templates

--- recursos/views.py
+++ recursos/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
 
-# Create your views here.
+from .models import Finishing, Material, Paper
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the Recursos index.")
+
+
+class FinishingsView(generic.ListView):
+    template_name = 'recursos/finishings.html'
+    context_object_name = 'latest_finishing_list'
+
+    def get_queryset(self):
+        """Return all the Finishings."""
+        return Finishing.objects.all()
+
+
+class MaterialsView(generic.ListView):
+    template_name = 'recursos/materials.html'
+    context_object_name = 'latest_material_list'
+
+    def get_queryset(self):
+        """Return all the Finishings."""
+        return Material.objects.all()
+
+
+class PapersView(generic.ListView):
+    template_name = 'recursos/papers.html'
+    context_object_name = 'latest_paper_list'
+
+    def get_queryset(self):
+        """Return all the Finishings."""
+        return Paper.objects.all()
