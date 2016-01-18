cd1a1f4fe44bf4bd0e3eaf2c3e6f42125a6bd2e7
Andres Sapper <asapper@hotmail.com>
2016-01-17 20:57:27 -0600

    make Recursos models modifiable in the admin

--- recursos/admin.py
+++ recursos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Finishing, Material, Paper
+
+admin.site.register(Finishing)
+admin.site.register(Material)
+admin.site.register(Paper)
