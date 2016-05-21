be2f862df66c4f7644067ad7ee121097dee10902
Andres Sapper <asapper@hotmail.com>
2016-05-21 01:40:33 -0500

    store Resources chosen in New or Edited Quote

--- ventas/views.py
+++ ventas/views.py
@@ -4,15 +4,28 @@ from django.views import generic
 
 from .forms import QuoteForm
 from .models import Quote
+from .models import Quote_Finishing
+from recursos.models import Finishing
 
 
 def new_quote(request):
+    """Creates a new quote based on the form returned."""
     if request.method == "POST":
         form = QuoteForm(request.POST)
         if form.is_valid():
             quote = form.save(commit=False)
-            quote.executive_id = 1
-            quote.save()
+            # assign executive that made this quote
+            quote.executive_id = 1  # FIX THIS!
+            quote.save()  # save quote
+            # store materials
+            quote.materials.set(form['materials'].value())
+            # store finishings
+            quote.finishings.clear()
+            for finishing_id in form['finishings'].value():
+                finishing = Finishing.objects.get(pk=finishing_id)
+                Quote_Finishing.objects.create(
+                    quote=quote,
+                    finishing=finishing)
             return redirect(reverse(
                 'ventas:quote_detail', kwargs={'pk': quote.id}))
     else:
@@ -21,12 +34,24 @@ def new_quote(request):
 
 
 def edit_quote(request, pk):
+    """Edits and updates a Quote based on the form returned."""
     quote = get_object_or_404(Quote, pk=pk)
     if request.method == "POST":
         form = QuoteForm(request.POST, instance=quote)
         if form.is_valid():
             quote = form.save(commit=False)
-            quote.executive_id = 1
+            # assign executive that made this quote
+            quote.executive_id = 1  # FIX THIS!
+            # clear old, keep only new list of materials
+            quote.materials.set(form['materials'].value())
+            # store finishings
+            quote.finishings.clear()
+            for finishing_id in form['finishings'].value():
+                finishing = Finishing.objects.get(pk=finishing_id)
+                Quote_Finishing.objects.create(
+                    quote=quote,
+                    finishing=finishing)
+            # save quote
             quote.save()
             return redirect(reverse(
                 'ventas:quote_detail', kwargs={'pk': quote.id}))
