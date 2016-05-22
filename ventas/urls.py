f7ab26fa826585aef4304755991b472af8af81a6
Andres Sapper <asapper@hotmail.com>
2016-05-22 15:28:24 -0500

    change Ventas views to class-based views

--- ventas/urls.py
+++ ventas/urls.py
@@ -9,10 +9,12 @@ urlpatterns = [
     # ex: /ventas/quotes/
     url(r'^quotes/$', views.QuotesView.as_view(), name='quotes'),
     # ex: /ventas/quotes/new
-    url(r'^quotes/new$', views.new_quote, name='new_quote'),
+    url(r'^quotes/new$', views.QuoteCreateView.as_view(),
+        name='quote_create'),
     # ex: /ventas/quotes/5/
     url(r'^quotes/(?P<pk>[0-9]+)/$', views.QuoteDetailView.as_view(),
         name='quote_detail'),
     # ex: /ventas/quotes/5/edit/
-    url(r'^quotes/(?P<pk>[0-9]+)/edit$', views.edit_quote, name='edit_quote'),
+    url(r'^quotes/(?P<pk>[0-9]+)/edit$', views.QuoteEditView.as_view(),
+        name='quote_edit'),
 ]
--- ventas/views.py
+++ ventas/views.py
@@ -1,5 +1,5 @@
 from django.core.urlresolvers import reverse
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import redirect
 from django.views import generic
 
 from .forms import QuoteForm
@@ -8,56 +8,53 @@ from .models import Quote_Finishing
 from recursos.models import Finishing
 
 
-def new_quote(request):
-    """Creates a new quote based on the form returned."""
-    if request.method == "POST":
-        form = QuoteForm(request.POST)
-        if form.is_valid():
-            quote = form.save(commit=False)
-            # assign executive that made this quote
-            quote.executive_id = 1  # FIX THIS!
-            quote.save()  # save quote
-            # store materials
-            quote.materials.set(form['materials'].value())
-            # store finishings
-            quote.finishings.clear()
-            for finishing_id in form['finishings'].value():
-                finishing = Finishing.objects.get(pk=finishing_id)
-                Quote_Finishing.objects.create(
-                    quote=quote,
-                    finishing=finishing)
-            return redirect(reverse(
-                'ventas:quote_detail', kwargs={'pk': quote.id}))
-    else:
-        form = QuoteForm()
-    return render(request, 'ventas/quote_edit.html', {'form': form})
+class QuoteCreateView(generic.edit.CreateView):
+    model = Quote
+    form_class = QuoteForm
+    template_name = 'ventas/quote_edit.html'
+
+    def form_valid(self, form):
+        """Process a valid form."""
+        quote = form.save(commit=False)
+        # assign executive that made this quote
+        quote.executive_id = 1  # FIX THIS!
+        quote.save()  # save quote
+        # store materials
+        quote.materials.set(form['materials'].value())
+        # store finishings
+        quote.finishings.clear()
+        for finishing_id in form['finishings'].value():
+            finishing = Finishing.objects.get(pk=finishing_id)
+            Quote_Finishing.objects.create(
+                quote=quote,
+                finishing=finishing)
+        return redirect(reverse(
+            'ventas:quote_detail', kwargs={'pk': quote.id}))
 
 
-def edit_quote(request, pk):
-    """Edits and updates a Quote based on the form returned."""
-    quote = get_object_or_404(Quote, pk=pk)
-    if request.method == "POST":
-        form = QuoteForm(request.POST, instance=quote)
-        if form.is_valid():
-            quote = form.save(commit=False)
-            # assign executive that made this quote
-            quote.executive_id = 1  # FIX THIS!
-            # clear old, keep only new list of materials
-            quote.materials.set(form['materials'].value())
-            # store finishings
-            quote.finishings.clear()
-            for finishing_id in form['finishings'].value():
-                finishing = Finishing.objects.get(pk=finishing_id)
-                Quote_Finishing.objects.create(
-                    quote=quote,
-                    finishing=finishing)
-            # save quote
-            quote.save()
-            return redirect(reverse(
-                'ventas:quote_detail', kwargs={'pk': quote.id}))
-    else:
-        form = QuoteForm(instance=quote)
-    return render(request, 'ventas/quote_edit.html', {'form': form})
+class QuoteEditView(generic.edit.UpdateView):
+    model = Quote
+    form_class = QuoteForm
+    template_name = 'ventas/quote_edit.html'
+
+    def form_valid(slef, form):
+        """Save an edited Quote."""
+        quote = form.save(commit=False)
+        # assign executive that made this quote
+        quote.executive_id = 1  # FIX THIS!
+        # clear old, keep only new list of materials
+        quote.materials.set(form['materials'].value())
+        # store finishings
+        quote.finishings.clear()
+        for finishing_id in form['finishings'].value():
+            finishing = Finishing.objects.get(pk=finishing_id)
+            Quote_Finishing.objects.create(
+                quote=quote,
+                finishing=finishing)
+        # save quote
+        quote.save()
+        return redirect(reverse(
+            'ventas:quote_detail', kwargs={'pk': quote.id}))
 
 
 class VentasView(generic.base.TemplateView):
